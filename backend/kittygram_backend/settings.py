89de9b49c4c3f63caf75948dce52c5fb55d0a8cf
Прылипко Егор <Egorprylipko@mail.ru>
2023-06-03 15:20:00 +0300

    1.8

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -100,8 +100,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
-MEDIA_URL = '/app/media/'
-MEDIA_ROOT = BASE_DIR / '/app/media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
